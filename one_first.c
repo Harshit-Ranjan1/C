1b6530ff04c67cf6dd60f3ae8152dbf9ec5034c8
Harshit Ranjan <harshit1ranjan@gmail.com>
2026-09-20 20:18:38 +0530

    news

--- one_first.c
+++ one_first.c
@@ -155,4 +155,6 @@ Click me to see the solution
 
 2. Sum of Positive Integers Until 0
 
-Write a C program that prompts the user to input a series of integers until the user stops by entering 0 using a do-while loop. Calculate and print the sum of all positive integers entered.*\
+Write a C program that prompts the user to input a series of integers until the user stops by entering 0 using a do-while loop. Calculate and print the sum of all positive integers entered.
+'Jhooth ki goonj deflated': Patra says ABVP’s DUSU win exposes 'hollowness' of Rahul’s narrative
+BJP MP Sambit Patra hailed the Akhil Bharatiya Vidyarthi Parishad (ABVP)'s performance in t*/
